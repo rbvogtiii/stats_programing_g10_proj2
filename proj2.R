1ed58dc187b077da26c055e91862708ac89860f1
Bradley Vogt <121974654+rbvogtiii@users.noreply.github.com>
2025-10-08 00:09:00 +0100

    alternate implementation for network exposures

--- proj2.R
+++ proj2.R
@@ -17,7 +17,7 @@ get.net <- function(beta, h, nc = 15) {
   n <- length(beta)
   link_const <- nc / ((mean(beta)**2) * (n - 1))
   probs <- matrix(beta %*% t(beta), n, n) * link_const # initialize probabilities for all pairings
-  probs[col(probs) >= row(probs)] <- 0 # get rid of double counting
+  probs[col(probs) >= row(probs) | h[col(probs) == h[row(probs)]]] <- 0 # get rid of double counting
   rands <- matrix(runif(n * n), n, n)
   probs[(probs - rands) >= 0] <- 1
   lapply(seq_along(beta), net_helper, probs)
@@ -56,25 +56,46 @@ nseir <- function(beta, h, alink, alpha = c(.1, .01, .01), delta = .2, gamma = .
     # E <- c(E, S[E_prob >= 0])
     # S <- S[E_prob < 0]
 
-    for (i in I) {
-      # each member of household exposed with prob alpha[1]
-      household <- S[S %in% alink[i]]
-      E_prob <- alpha[1] - runif(length(household))
-      E <- c(E, household[E_prob >= 0])
+    if (length(I) == 0) {
+      next
+    }
 
-      # each member of network exposed with prob alpha[2]
-      network <- S[S %in% alink[i]]
-      E_prob <- alpha[2] - runif(length(network))
-      E <- c(E, network[E_prob >= 0])
+    # alternative for household exposures
+    E_prob <- 1 - (((1 - alpha[1])**tabulate(h[pop %in% I], nbins = max(h)))[h[S]]) - runif(length(S))
+    E <- c(E, S[E_prob >= 0])
+    S <- S[E_prob < 0]
+
+    if (length(I) == 0) {
+      next
+    }
+
+    # alternative for network exposures
+    E_prob <- 1 - ((1 - alpha[2])**tabulate(unlist(alink[I]), nbins = n)[S]) - runif(length(S))
+    E <- c(E, S[E_prob >= 0])
+    S <- S[E_prob < 0]
+
+    for (i in I) {
+      # # each member of household exposed with prob alpha[1]
+      # household <- S[h[S] == h[i]]
+      # E_prob <- alpha[1] - runif(length(household))
+      # E <- c(E, household[E_prob >= 0])
+
+      # # each member of network exposed with prob alpha[2]
+      # network <- S[S %in% alink[i]]
+      # E_prob <- alpha[2] - runif(length(network))
+      # E <- c(E, network[E_prob >= 0])
 
       # random ppl exposed w prob beta[i]*beta[j]*infection_const
       E_prob <- (beta[i] * beta[pop %in% S] * infection_const) - runif(length(S))
       E <- c(E, S[E_prob >= 0])
 
-      # maybe move this out?
-      E <- unique(E)
-      S <- S[!(S %in% E)]
+      # # maybe move this out of loop?
+      # E <- unique(E)
+      # S <- S[!(S %in% E)]
     }
+
+    E <- unique(E)
+    S <- S[!(S %in% E)]
 
     S_out <- c(S_out, length(S))
     E_out <- c(E_out, length(E))
@@ -96,9 +117,9 @@ beta <- runif(n)
 alink <- get.net(beta, h, nc)
 # print(system.time())
 # print("here")
-# s1 <- nseir(beta, h, alink, pinf = 0.5, nt = 2)
+# print(system.time(s1 <- nseir(beta, h, alink, pinf = 0.5, nt = 2)))
 
-# s1 <- nseir(beta, h, alink)
+print(system.time(s1 <- nseir(beta, h, alink)))
 # s2 <- nseir(beta, h, alink, alpha = c(0, 0, 0.04))
 # const_beta <- rep(mean(beta), length(beta))
 # s3 <- nseir(const_beta, h, alink)
@@ -110,9 +131,9 @@ alink <- get.net(beta, h, nc)
 # hist(beta)
 # hist(const_beta)
 # hist(const_beta)
-plot(s1$t, s1$S, xlab = "day", ylim = c(0, n))
-points(s1$t, s1$I, col = 4)
-points(s1$t, s1$E, col = 3)
-points(s1$t, s1$R, col = 2)
+# plot(s1$t, s1$S, xlab = "day", ylim = c(0, n))
+# points(s1$t, s1$I, col = 4)
+# points(s1$t, s1$E, col = 3)
+# points(s1$t, s1$R, col = 2)
 # plot(s2$t, s2$S, xlab = "day", ylim = c(0, n))
 # plot(s3$t, s3$S, xlab = "day", ylim = c(0, n))
